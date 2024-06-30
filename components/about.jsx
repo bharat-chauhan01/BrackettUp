4bd1af9e995bd82fd4fa019444cf86ac13c5adc8
cooldd2171 <shourya.rastogi436@gmail.com>
2024-06-30 10:33:42 +0200

    added color

--- components/about.jsx
+++ components/about.jsx
@@ -8,7 +8,7 @@ export default function About() {
       <Text style={[GlobalStyle.heading]}>
         About us
       </Text>
-      <Text style={{ fontSize: 17, fontWeight: '300', paddingVertical: 17 }}>
+      <Text style={{ fontSize: 17, fontWeight: '300', paddingVertical: 17, color: 'black' }}>
         HIIT Bootcamp Vondelpark is a dynamic outdoor fitness experience that combines high-intensity interval
         training with bodyweight exercises and resistance band work. Our expert instructors create energizing workout sessions set to motivating beats in the beautiful surroundings of Amsterdam's Vondelpark. We believe in promoting overall health and wellness through challenging yet fun group fitness classes that build strength, endurance, and a supportive community spirit. Whether you're a seasoned athlete or just starting your fitness journey, our inclusive bootcamps provide an invigorating full-body workout tailored to different fitness levels. 
         Join us amidst the park's greenery for an unbeatable exercise experience that leaves you feeling empowered and rejuvenated.
--- components/cancellationPolicy.jsx
+++ components/cancellationPolicy.jsx
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function CancellationPolicy() {
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Cancellation policy</Text>
+      <Text style={{ fontSize: 24, fontWeight: 'bold', color: 'black' }}>Cancellation policy</Text>
       <Text style={{ fontSize: 17, fontWeight: '300', paddingVertical: 17 }}>
         Cancel 12 hours in advance to avoid a $8.00 late cancellation fee. A missed reservation will result in a $11.00 fee.
       </Text>
