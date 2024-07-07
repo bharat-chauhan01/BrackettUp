83a27f7b9f5fb4655795285c7346102d7a4224e2
Mukesh Rana <mukeshrana.e.eng@gmail.com>
2024-07-07 13:47:42 +0530

    Mr/splash screen (#3)
    * added splash screen
    
    * changes related to AsyncStorage
    
    * made changes for readability
    
    * removed extra space and renamed variable name
    
    * rename variables
    
    * rename storage key
    
    ---------
    
    Co-authored-by: Ashish Rana <ashishrana@Ashishs-MacBook-Air.local>

--- App.jsx
+++ App.jsx
@@ -4,14 +4,47 @@ import { Provider } from 'react-redux';
 import LandingScreen from './screens/LandingScreen';
 import Login from './screens/login';
 import { store } from './store/redux/store';
+import SplashScreen from 'react-native-splash-screen';
+import { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [landingPage, setLandingPage] = useState(null);
+
+  useEffect(() => {
+    // Retrieve data when the component mounts
+    const getData = async () => {
+      try {
+        const firstTimeUser = await AsyncStorage.getItem('firstTimeUser')
+        if (firstTimeUser !== null) {
+          setLandingPage("Landing")
+        }
+        else{ 
+          await AsyncStorage.setItem('firstTimeUser',"loggedIn")
+          setLandingPage("Login")
+        }
+      } catch (e) {
+        console.log("Exception during fetching data from AsyncStorage :: ", e)
+        setLandingPage("Login");
+      }
+      finally{ 
+          SplashScreen.hide();
+      }
+    };
+    getData();
+  }, []);
+
+  if (landingPage === null) {
+    return null;
+  }
+
   return (
     <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator
+          initialRouteName={landingPage}
           screenOptions={{
             headerStyle: { backgroundColor: '#351401' },
             headerTintColor: 'white',
