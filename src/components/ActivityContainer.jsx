fc967c631cbe9ad5b4b733818d9d71adb269acd5
Diti Banerjee <99901175+Diti02@users.noreply.github.com>
2024-07-13 14:19:30 +0530

    activity container (#6)
    * activity container
    
    * activity container update1
    
    * update2
    
    * update3
    
    * utils created
    
    * icon styles update

---
+++ src/components/ActivityContainer.jsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { capitalizeFirstLetter } from '../utils';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+const fontSize = 15;
+
+const ActivityContainer = ({
+  imageUrl,
+  title,
+  subtitle,
+  activity,
+  distance,
+  rating,
+  ratingCount,
+  ratingDesc,
+}) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: imageUrl }} style={styles.image} />
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{capitalizeFirstLetter(title)}</Text>
+        <View style={styles.act}>
+          <Text style={styles.subtitle}>{capitalizeFirstLetter(subtitle)}</Text>
+          <Text style={styles.distance}>{distance}</Text>
+        </View>
+        <Text style={styles.activity}>{capitalizeFirstLetter(activity)}</Text>
+        <View style={styles.ratingContainer}>
+          <Text style={styles.ratingText}>
+            {rating}{' '}
+            <MaterialCommunityIcons
+              name={styles.itemIcon.name}
+              color={styles.itemIcon.color}
+              size={styles.itemIcon.size}
+            />
+          </Text>
+          <Text style={styles.ratingCount}>{ratingCount}</Text>
+          <Text style={styles.ratingDesc}>{capitalizeFirstLetter(ratingDesc)}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: windowWidth * 0.425,
+    height: windowHeight * 0.24,
+    borderRadius: 10,
+    overflow: 'hidden',
+    backgroundColor: '#fff',
+    marginHorizontal: '0.5%',
+    padding: '1.5%',
+  },
+  image: {
+    width: '100%',
+    height: '53%',
+    borderRadius: 10,
+  },
+  textContainer: {
+    marginTop: '2%',
+  },
+  title: {
+    fontSize: windowWidth * 0.04,
+    fontWeight: 'bold',
+    color: 'black',
+    letterSpacing: 1.25,
+  },
+  subtitle: {
+    fontSize: windowWidth * 0.035,
+    color: '#666',
+  },
+  act: {
+    flexDirection: 'row',
+  },
+  ratingContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    gap: windowWidth * 0.005,
+  },
+  rating: {
+    fontSize: windowWidth * 0.033,
+    color: '#666',
+  },
+  itemIcon: {
+    name: 'star',
+    color: '#000000',
+    size: fontSize,
+  },
+  activity: {
+    fontSize: windowWidth * 0.035,
+    color: '#666',
+  },
+  ratingText: {
+    fontSize: windowWidth * 0.035,
+    color: 'black',
+    marginTop: '1%',
+    fontWeight: 'bold',
+  },
+  ratingDesc: {
+    fontSize: windowWidth * 0.033,
+    color: 'purple',
+    fontWeight: '600',
+  },
+});
+
+export default ActivityContainer;
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,17 +1,75 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
+import ActivityContainer from '../components/ActivityContainer';
 
 export default function HomeScreen() {
+  const activities = [
+    {
+      imageUrl:
+        'https://classpass-res.cloudinary.com/image/upload/f_auto/q_auto/zf79zuoxsozfzovlgolf.jpg',
+      title: 'Rocycle',
+      subtitle: 'Zuidas · ',
+      distance: '0.6 km',
+      activity: 'Cycling',
+      rating: '4.7',
+      ratingCount: '(30000+)',
+      ratingDesc: 'Excellent',
+    },
+    {
+      imageUrl:
+        'https://media.istockphoto.com/id/1483989758/photo/diverse-yoga-class-participants-doing-a-side-plank-on-their-yoga-mats-in-a-beautiful-yoga.jpg?s=1024x1024&w=is&k=20&c=EQE_TpZPMPPkjtW94XIqXxSBW69EZ4c-b2rhOmUcUcY=',
+      title: 'YogaSpot',
+      subtitle: 'Buitenveldert · ',
+      distance: '0.6 km',
+      activity: 'Yoga',
+      rating: '4.5',
+      ratingCount: '(20000+)',
+      ratingDesc: 'Great',
+    },
+    {
+      imageUrl:
+        'https://classpass-res.cloudinary.com/image/upload/f_auto/q_auto/ggjnlugk1n9dgcgczibs.jpg',
+      title: 'ClubSportive',
+      subtitle: 'Zuidas · ',
+      distance: '0.5 km',
+      activity: 'Gym Time',
+      rating: '4.7 ',
+      ratingCount: '(30000+)',
+      ratingDesc: 'Great',
+    },
+  ];
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: 'center',
+        // justifyContent: 'center',
         alignItems: 'center',
         color: 'black',
       }}
     >
       <Text>Welcome to Home Screen</Text>
+      <View
+        style={{
+          padding: 12,
+          // marginStart: 10,
+        }}
+      >
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {activities.map((activity, index) => (
+            <ActivityContainer
+              key={index}
+              imageUrl={activity.imageUrl}
+              title={activity.title}
+              subtitle={activity.subtitle}
+              distance={activity.distance}
+              activity={activity.activity}
+              rating={activity.rating}
+              ratingCount={activity.ratingCount}
+              ratingDesc={activity.ratingDesc}
+            />
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 }
