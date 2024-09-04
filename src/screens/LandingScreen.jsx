ed0806c121d5bbf9372975efa2f844a3613e40b9
cooldd2171 <shourya.rastogi436@gmail.com>
2024-09-04 13:26:55 +0200

    fixed landing screen

--- src/screens/LandingScreen.jsx
+++ src/screens/LandingScreen.jsx
@@ -32,7 +32,6 @@ export default function LandingScreen() {
           height: navBarHeight,
           paddingBottom: navBarPaddingBottom,
           backgroundColor: 'white', 
-          position: 'absolute',
           borderTopColor: 'transparent', 
         },
       }}
